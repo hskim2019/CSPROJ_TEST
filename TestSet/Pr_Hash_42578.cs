fb42318d5725a1edf3ceaea2c259b32f1fa3140b
hskim2019 <hwasun.kim2019@gmail.com>
2025-11-03 00:05:07 +0900

    .

---
+++ TestSet/Pr_Hash_42578.cs
@@ -0,0 +1,13 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=csharp
+
+using System;
+
+public class Solution
+{
+    public int solution(string[,] clothes)
+    {
+        int answer = 0;
+        return answer;
+    }
+}
