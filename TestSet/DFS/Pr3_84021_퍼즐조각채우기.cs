0f575f84b869fde254f070ffedc935d6defee169
hskim2019 <hwasun.kim2019@gmail.com>
2026-01-08 00:36:51 +0900

    문제.

---
+++ TestSet/DFS/Pr3_84021_퍼즐조각채우기.cs
@@ -0,0 +1,15 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/84021?language=csharp
+
+using System;
+
+var int[,] game_board = [[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]];
+var int[,] table = [[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],[0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]];
+// result = 14
+
+
+public class Solution {
+    public int solution(int[,] game_board, int[,] table) {
+        int answer = -1;
+        return answer;
+    }
+}
