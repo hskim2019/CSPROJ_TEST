d61e9959129b24f56663ba729a31e392f4a29358
hskim2019 <hwasun.kim2019@gmail.com>
2026-01-08 01:08:48 +0900

    .

--- TestSet/DFS/Pr3_84021_퍼즐조각채우기.cs
+++ TestSet/DFS/Pr3_84021_퍼즐조각채우기.cs
@@ -1,5 +1,7 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/84021?language=csharp
-
+// https://doublsb.tistory.com/124
+// https://howudong.tistory.com/158
+// https://velog.io/@soseuleaf/%ED%8D%BC%EC%A6%90-%EC%A1%B0%EA%B0%81-%EC%B1%84%EC%9A%B0%EA%B8%B0
 using System;
 
 var int[,] game_board = [[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]];
