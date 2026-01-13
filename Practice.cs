f44d273fb052d837b8744d99a1b20f6e633ddf99
hskim2019 <hwasun.kim2019@gmail.com>
2026-01-13 18:09:29 +0900

    .

--- Practice.cs
+++ Practice.cs
@@ -4,6 +4,9 @@ public class Solution
 {
     public int solution(int[] citations)
     {
-
+        foreach (var c in citations)
+        {
+            var cnt = citations.Contains(c);
+        }
     }
 }
