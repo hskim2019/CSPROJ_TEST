a4d836f8133db1578a7670679c642ac5732e23a9
hskim2019 <hwasun.kim2019@gmail.com>
2026-01-15 23:59:38 +0900

    42474

--- TestSet/Sorting/Pr2_42747.cs
+++ TestSet/Sorting/Pr2_42747.cs
@@ -15,3 +15,30 @@
 // [3, 0, 6, 1, 5]	3
 // 입출력 예 설명
 // 이 과학자가 발표한 논문의 수는 5편이고, 그중 3편의 논문은 3회 이상 인용되었습니다. 그리고 나머지 2편의 논문은 3회 이하 인용되었기 때문에 이 과학자의 H-Index는 3입니다.
+
+using System;
+
+int[] citations = { 3, 0, 6, 1, 5 };
+var sol = new Solution();
+Console.WriteLine(sol.solution(citations));
+public class Solution
+{
+    public int solution(int[] citations)
+    {
+        int answer = 0;
+        Array.Sort(citations, (a, b) => b.CompareTo(a));
+        for (var i = 0; i < citations.Length; i++)
+        {
+            if (citations[i] >= (i + 1))
+            {
+                answer += 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
