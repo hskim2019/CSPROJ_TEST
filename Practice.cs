668f8253aa115d1e031fc43019db0ff6a8a56870
hskim2019 <hwasun.kim2019@gmail.com>
2026-01-18 13:29:30 +0900

    .

--- Practice.cs
+++ Practice.cs
@@ -1,16 +1,37 @@
-string[] phone_book = { "123", "456", "789" };
-var sol = new Solution();
-Console.WriteLine(sol.solution(phone_book));
-class Solution
+using System;
+
+var genres = { "classic", "pop", "classic", "classic", "pop" };
+var plays = { 500, 600, 150, 800, 2500 };
+// [4, 1, 3, 0]
+
+public class Solution
 {
-    public bool solution(String[] phone_book)
+    public class Song
     {
-        Array.Sort(phone_book);
-        for (var i = 0; i < phone_book.Length - 1; i++)
+        public int Index { get; set; }
+        public int Plays { get; set; }
+    }
+    public int[] solution(string[] genres, int[] plays)
+    {
+        var dict = new Dictionary<string, List<Song>>();
+        for (int i = 0; i < genres.Length; i++)
         {
-            if (phone_book[i + 1].StartsWith(phone_book[i]))
-                return false;
+            if (!dict.ContainsKey(genres[i]))
+                dict[genres[i]] = new List<Song>();
+
+            dict[genres[i]].Add(new Song { Index = i, Plays = plays[i] });
+
         }
-        return true;
+
+        var sortedGenres = dict.OrderByDescending(p => p.Value.Sum(song => song.Plays));
+        var answer = new List<int>();
+
+        foreach (var genre in sortedGenres)
+        {
+            var topSongs = genre.Value.OrderByDescending(s => s.Plays)
+            .ThenBy(s => s.Index).Take(2);
+            answer.AddRange(topSongs.Select(s => s.Index));
+        }
+        return answer.ToArray();
     }
 }
--- TestSet/Hash/Pr2_Hash_42579.cs
+++ TestSet/Hash/Pr2_Hash_42579.cs
@@ -15,14 +15,17 @@
 // 장르에 속한 곡이 하나라면, 하나의 곡만 선택합니다.
 // 모든 장르는 재생된 횟수가 다릅니다.
 // 입출력 예
-// genres	plays	return
-// ["classic", "pop", "classic", "classic", "pop"]	[500, 600, 150, 800, 2500]	[4, 1, 3, 0]
+// genres = {"classic", "pop", "classic", "classic", "pop"}
+// plays = {500, 600, 150, 800, 2500}
+// return = {4, 1, 3, 0}
+
 // 입출력 예 설명
 // classic 장르는 1,450회 재생되었으며, classic 노래는 다음과 같습니다.
 
 // 고유 번호 3: 800회 재생
 // 고유 번호 0: 500회 재생
 // 고유 번호 2: 150회 재생
+
 // pop 장르는 3,100회 재생되었으며, pop 노래는 다음과 같습니다.
 
 // 고유 번호 4: 2,500회 재생
@@ -31,6 +34,53 @@
 
 // 장르 별로 가장 많이 재생된 노래를 최대 두 개까지 모아 베스트 앨범을 출시하므로 2번 노래는 수록되지 않습니다.
 
-var genres = {"classic", "pop", "classic", "classic", "pop"};
-var plays = {500, 600, 150, 800, 2500};
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+var genres = { "classic", "pop", "classic", "classic", "pop" };
+var plays = { 500, 600, 150, 800, 2500 };
 // [4, 1, 3, 0]
+
+public class Solution
+{
+    // 곡 정보를 담는 클래스
+    public class Song
+    {
+        public int Index { get; set; }
+        public int Plays { get; set; }
+    }
+
+    public int[] solution(string[] genres, int[] plays)
+    {
+        var dict = new Dictionary<string, List<Song>>();
+
+        // 장르별 곡 리스트 구성
+        for (int i = 0; i < genres.Length; i++)
+        {
+            if (!dict.ContainsKey(genres[i]))
+                dict[genres[i]] = new List<Song>();
+
+            dict[genres[i]].Add(new Song { Index = i, Plays = plays[i] });
+        }
+
+        // 장르별 총 재생 횟수 기준으로 정렬
+        var sortedGenres = dict
+            .OrderByDescending(g => g.Value.Sum(song => song.Plays));
+
+        var answer = new List<int>();
+
+        // 각 장르에서 최대 2곡 선택
+        foreach (var genre in sortedGenres)
+        {
+            var topSongs = genre.Value
+                .OrderByDescending(s => s.Plays) // 재생 횟수 내림차순
+                .ThenBy(s => s.Index)            // index 오름차순
+                .Take(2);
+
+            answer.AddRange(topSongs.Select(s => s.Index));
+        }
+
+        return answer.ToArray();
+    }
+}
