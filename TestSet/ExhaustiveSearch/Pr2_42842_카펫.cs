3c3decc6d9a4738f7198249150c87490e6f0ed3e
hskim2019 <hwasun.kim2019@gmail.com>
2026-03-13 17:10:27 +0900

    .

--- TestSet/ExhaustiveSearch/Pr2_42842_카펫.cs
+++ TestSet/ExhaustiveSearch/Pr2_42842_카펫.cs
@@ -13,13 +13,27 @@
 // 갈색 격자의 수 brown은 8 이상 5,000 이하인 자연수입니다.
 // 노란색 격자의 수 yellow는 1 이상 2,000,000 이하인 자연수입니다.
 // 카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
-// 입출력 예
-// brown	yellow	return
-// 10	2	[4, 3]
-// 8	1	[3, 3]
-// 24	24	[8, 6]
 
 using System;
+
+int brown = 10;
+int yellow = 2;
+// [4, 3]
+
+// int brown = 8;
+// int yellow = 1;
+// [3, 3]
+
+// int brown = 24;
+// int yellow = 24;
+// [8, 6]
+
+var sol = new Solution();
+
+int brown = 10;
+int yellow = 2;
+
+Console.WriteLine(string.Join(" , ", sol.solution(brown, yellow)));
 
 public class Solution
 {
